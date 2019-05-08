05c9cbc2075afd459c5ac31fd1b3eac1fc297e1f
张晓钒 <zxv970209@outlook.com>
2019-05-08 16:06:55 +0800

    Feature: 修改音频中增加了暂停和显示播放时间

--- src/components/AudioChoose/index.js
+++ src/components/AudioChoose/index.js
@@ -5,6 +5,7 @@ import { Howl } from 'howler';
 import { connect } from 'react-redux';
 import { updateMp3 } from "../../redux/detail_redux";
 import { AudioMapper } from "../../utils/audio";
+import { ButtonCompont } from '../../pages/Admin/DetailControl/style';
 
 const { Option } = Select;
 
@@ -19,40 +20,89 @@ class AudioChoose extends Component{
         super(props);
         this.state = {
             mp3: '',
+            Sounds: '',
+            pause: false,
         }
     }
     componentDidMount(){
         const { mp3 } = this.props.location.state;
+        const Sounds = new Howl({
+            src: [require(`../../assets/mp3/${mp3}.mp3`)],
+            onend: () => {
+                this.setState({pause: false})
+            }
+        })
         this.setState({
             mp3: mp3,
+            Sounds,
         })
     }
     SoundPlay() {
-        const { mp3 } = this.state;
-        const Sounds = new Howl({
-            src: [require(`../../assets/mp3/${mp3}.mp3`)],
-        })
-        Sounds.play()
+        const { Sounds,pause } = this.state;
+        if(pause){
+            Sounds.pause()
+            this.setState({pause:false})
+        }else{
+            Sounds.play()
+            this.setState({pause:true})
+        }
     }
     handleChange(value) {
+        const { Sounds } = this.state;
+        Sounds.pause()
+        const sounds = new Howl({
+            src: [require(`../../assets/mp3/${value}.mp3`)],
+            onend: () => {
+                this.setState({pause: false})
+            }
+        })
         this.setState({
             mp3: value,
+            Sounds: sounds,
+            pause: false,
         })
         console.log(value);
     }
     updatemp3(id){
         this.props.updateMp3(id,this.state)
     }
+    playTime(){
+        const { Sounds,pause } = this.state;
+        if(Sounds){
+            if(typeof Sounds.seek() === 'number'){
+                return (
+                    <div style={{ marginTop:15 }}>
+                        <p style={Sounds&&!pause? null:{display:'none'}}>{Sounds&&!pause?`已经播放`+Sounds.seek().toFixed(0)+`秒`: null}</p>
+                        <p style={pause? null : {display:'none'}}>{pause?`正在播放`: null}</p>
+                    </div>
+                )
+            }else{
+                return (
+                    <div style={{ marginTop:15 }}>
+                        <p>已经播放0秒</p>
+                    </div>
+                )
+            }
+            
+        }
+    }
+    back(){
+        //返回是如果音频还播放就关掉它
+        const { Sounds } = this.state;
+        Sounds.pause()
+    }
     render() {
         const { id,path,detailname,pagenumber } = this.props.location.state;
-        const { mp3 } = this.state;
-        
+        const { mp3,pause } = this.state;
+
         return (
             <div>
-                <Link to={{
-                    pathname: path,
-                    pagenumber: pagenumber,
-                }}>
+                <Link 
+                    onClick={()=>this.back()}
+                    to={{
+                        pathname: path,
+                        pagenumber: pagenumber,
+                    }}>
                     <Icon type="caret-left"/>
                     Back
                 </Link>
@@ -78,9 +128,16 @@ class AudioChoose extends Component{
                             </span>
                         </div>
                     </Col>
-                    <Col span={4} push={8}>
+                    <Col span={4} push={10}>
+                        {this.playTime()}
+                    </Col>
+                    <Col span={4} push={6}>
                         <div style={{ marginTop:10,marginBottom:20 }}>
-                            <Button type="primary" onClick={() => this.SoundPlay()}>播放音频</Button>
+                            <ButtonCompont
+                                className={pause ? 'pause' : null }
+                                type="primary"
+                                onClick={() =>this.SoundPlay()}
+                            >{pause ? <div>暂停音频</div> : <div>播放音频</div>}</ButtonCompont>
                         </div>
                     </Col>
                     <Col span={4} push={2}>
